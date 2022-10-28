064b36b30d7725c0217ef07c22273ccae9847b85
Nahum Martinez <nahum.martinez@sedena.local>
2022-10-28 16:59:19 -0600

    NAM

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" href="#">
+          Logo
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarsExampleDefault"
+          aria-controls="navbarsExampleDefault"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+          <ul class="navbar-nav mr-auto"></ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input
+              class="form-control mr-sm-2"
+              type="text"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button class="btn btn-secondary my-2 my-sm-0" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+
+      <main role="main" class="container">
+        <div class="starter-template">
+          <img src="img/meme.jpg" alt="" srcset="" />
+          <p class="lead">Éxitos</p>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
