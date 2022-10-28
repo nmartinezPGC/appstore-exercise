80ef32329bc765646cccc802aeeed307a7a9bf3a
Nahum Martinez <nahum.martinez@sedena.local>
2022-10-28 17:19:33 -0600

    NAM

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <>
       <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
         <a class="navbar-brand" href="#">
-          Logo
+          <img src="public/logo.png" />
         </a>
         <button
           class="navbar-toggler"
